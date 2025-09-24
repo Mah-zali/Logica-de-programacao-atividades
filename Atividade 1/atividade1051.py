bc1f64962673b5ce05e31f151adc209ca1fb8a7d
Mah-zali <mariana.mazali@sempreceub.com>
2025-09-23 21:35:55 -0300

    Update atividade1051.py

--- Atividade 1/atividade1051.py
+++ Atividade 1/atividade1051.py
@@ -8,7 +8,7 @@ imposto1 = (salario - 2000.00) * 0.08
 imposto2 = (1000.00 * 0.08)+((salario-3000.00)*0.18)
 #paga 8% do que passar de 2000 e mais 18% do que passar de 3000
 
-imposto3 = (1000.00 * 0.08)+(1500.00*0.018)+((salario-3000.00)*0.18)
+imposto3 = (1000.00 * 0.08)+(1500.00*0.18)+((salario-3000.00)*0.18)
 #paga 8% do que passar de 2000, mais 18% do que passar de 3000 e mais 28% do que passar de 4500
 
 if salario <= 2000.00:
@@ -19,4 +19,5 @@ else:
     if salario <= 4500.00:
         print(f"R$ {imposto2:.2f}")
     if salario > 4500.00:
+
         print(f"R$ {imposto3:.2f}")
